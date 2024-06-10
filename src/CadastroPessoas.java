338bcef34fdb726d6de890a726ad23a9212f101d
Jhonatan Negri <jhonatannegri659@gmail.com>
2024-06-10 20:58:58 -0300

    Adicionar pessoa

--- src/CadastroPessoas.java
+++ src/CadastroPessoas.java
@@ -3,8 +3,17 @@ import java.util.ArrayList;
 public class CadastroPessoas {
 
     private ArrayList<String> pessoas = new ArrayList<String>();
-    
+
+    public void adicionar(String nome) {
+        pessoas.add(nome);
+        System.out.println(nome + " adicionado(a) com sucesso!");
+    }
+
     public static void main(String[] args) {
+        CadastroPessoas cadastro = new CadastroPessoas();
+
+        cadastro.adicionar("Beatriz");
+        cadastro.adicionar("Carlos");
 
     }
 }
